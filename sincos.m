7a1d07e773afd811d123739eec4963741b93e0f7
Dzeneta Buivydaite <dzeneciux@gmail.com>
2013-03-21 11:58:44 +0200

    Sukureme skripta sincos grafikui

--- sincos.m
+++ sincos.m
@@ -2,6 +2,7 @@ function fsincos=sincos(x);
 %Grazina funkcijos: f=sin(x)+cos(x) reiksme.
 %Paleidimas: f=sincos(x), kur x vienmatis vektorius.
 %Pvz.: f=sincos(1.0);
+%Leisitnoji sritis (-10,10)
 fsincos=sin(x)+cos(x);
 
 end
