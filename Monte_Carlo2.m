36598d5a2865567c9dd2261a9f58a336df5bd5d5
Dzeneta Buivydaite <dzeneciux@gmail.com>
2013-03-28 12:29:05 +0200

    Realizavome dvimaty Monte_Carlo

---
+++ Monte_Carlo2.m
@@ -0,0 +1,22 @@
+%Monte Carlo (Random search) metodo realizacija.
+%Sugeneruojame 100 atsitiktiniu tasku is intervalo  [a,b].
+%Surandame kuriame taske funkcija igyja didziausia ir maziausia reiksme.
+a = -10; 
+b = 10;
+n=2;%Dimensija (matavimas)
+k=100;%Tasku (vektoriu) skaicius
+x = a + (b-a) .* rand(k,n) %Perdaryti, kad generuotu dvimacius vektorius
+for i=1:k
+f(i)=sincos2(x(i,:));
+end
+[fMin,indMin]=min(f);
+[fMax,indMax]=max(f);
+xMin=x(indMin,:);
+xMax=x(indMax,:);
+fprintf('Surastas min=%6.4f, taske x=(%6.4f, %6.4f)\n',fMin,xMin(1),xMin(2));
+fprintf('Surastas max=%6.4f, taske x=(%6.4f, %6.4f)\n',fMax,xMax(1),xMax(2));
+grafikas2
+
+
+
+
---
+++ sincos2.m
@@ -0,0 +1,9 @@
+function fsincos2=sincos2(x);
+%Grazina funkcijos: f=sin(x)+cos(x) reiksme.
+%Paleidimas: f=sincos(x), kur x vienmatis vektorius.
+%Pvz.: f=sincos2([1.0,1.0]);
+%Leisitnoji sritis(poaibis apibr4=imo srities) [-10,10]^2 arba
+%([-10,10]x[-10,10]);
+fsincos2=sin(x(1))+cos(x(2));
+
+end
