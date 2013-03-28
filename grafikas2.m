95e0b0392f74a184acd1ef98d2cbe4c6abc53796
Dzeneta Buivydaite <dzeneciux@gmail.com>
2013-03-28 12:57:26 +0200

    Sukuriau programa, kuri brezia dvimates funkcijos grafika

--- grafikas2.m
+++ grafikas2.m
@@ -1,5 +1,3 @@
-x1=-10:0.2:10;%Padaryti, kad sugeneruotu dvimaciu tasku tinkleli
-x2=x1;
-[X1,X2]=meshgrid(x1,x2);
-fsincos2=sincos2(x);
-mesh(X1,X2,fsincos2)%plote netiks, rikia naudoti komandas surf, mesh
+[x,y]=meshgrid(-10:0.2:10);
+f=sin(x)+cos(y)
+surf(x,y,f)%plote netiks, rikia naudoti komandas surf, mesh
