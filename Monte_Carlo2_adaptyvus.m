e8c684f98d3f8e91455d01684f00de50f01448e2
Dzeneta Buivydaite <dzeneciux@gmail.com>
2013-04-11 12:55:28 +0300

    Veikiantis, bet nebaigtas adaptyvusis Monte_Carlo2

--- Monte_Carlo2_adaptyvus.m
+++ Monte_Carlo2_adaptyvus.m
@@ -1,45 +1,69 @@
 %Monte Carlo (Random search) metodo realizacija.
 %Sugeneruojame 100 atsitiktiniu tasku is intervalo  [a,b].
 %Surandame kuriame taske funkcija igyja didziausia ir maziausia reiksme.
-a = -10; 
-b = 10;
+a1 = -10; % Pradine sritis
+b1 = 10;
 n=2;%Dimensija (matavimas)
-%k=70;%Tasku (vektoriu) skaicius
-%Intervalas bus skaidomas i k1=70 ir k2=30
+k1=50;%Tasku (vektoriu) skaicius, generuojame 50 tasku
+%Intervalas bus skaidomas i k1=50 ir k2=50
 %intervalas bus mazinamas
-x = a + (b-a) .* rand(70,n); %Perdaryti, kad generuotu dvimacius vektorius
-for i=1:70
-f(i)=sincos2(x(i,:));%Imame visa eilute, perduodame reiksmes x1, x2
+x1 = a1 + (b1-a1) .* rand(k1,n); %Perdaryti, kad generuotu dvimacius vektorius
+f1=[];
+for i=1:k1
+f1(i)=sincos2(x1(i,:));%Imame visa eilute, perduodame reiksmes x1, x2
 end
 %Ieskosime min ir max
-[fMin,indMin]=min(f);
-[fMax,indMax]=max(f);
+[fMin1,indMin1]=min(f1);
+%[fMax1,indMax1]=max(f1);
 %Surandame, kurioje eiluteje yra musu ieskoma reiksme
-xMin=x(indMin,:);%paimama visa eilute su abiem reiksmem
-xMax=x(indMax,:);%paimama visa eilute su abiem reiksmem
+xMin1=x1(indMin1,:);%paimama visa eilute su abiejomis reiksmemis
+%xMax1=x1(indMax1,:);%paimama visa eilute su abiejomis reiksmemis
 %Surastas reiksmes isvedame ekrane
-fprintf('Surastas min=%6.4f, taske x=(%6.4f, %6.4f)\n',fMin,xMin(1),xMin(2));
-fprintf('Surastas max=%6.4f, taske x=(%6.4f, %6.4f)\n',fMax,xMax(1),xMax(2));
-grafikas2
+fprintf('Surastas min1=%6.4f, taske x1=(%6.4f, %6.4f)\n',fMin1,xMin1(1),xMin1(2));
+%fprintf('Surastas max1=%6.4f, taske x1=(%6.4f, %6.4f)\n',fMax1,xMax1(1),xMax1(2));
+%grafikas2
+
+hold on;
+scatter(x1(:,1),x1(:,2),'b.');
+scatter(xMin1(1),xMin1(2),'r*');
+text(xMin1(1)+0.3,xMin1(2),num2str(fMin1));
+rectangle('Position',[-10.0,-10.0,20.0,20.0],...
+    'Linewidth',5,'Linestyle','--')
+
 %Skaiciuojame mazesniame intervale
-a1=-5;
-b1=5;
-n=2;%dimensija(matavimas)
-%k=30;
-%Skaidome intervala i k1=70 ir k2=30
-x=a1+(b1-a1).*rand(30,n)
-for i=1:30
-    f(i)=sincos2(x(i,:));
+
+a2=xMin1(1)-4;
+b2=xMin1(1)+4;
+a3=xMin1(2)-4;
+b3=xMin1(2)+4;
+
+%n=2;%dimensija(matavimas)
+k2=50;%likusiu tasku (vektoriu skaicius)
+x2(:,1)=a2+(b2-a2).*rand(k2,1);
+x2(:,2)=a3+(b3-a3).*rand(k2,1);
+f2=[];
+for i=1:k2
+    f2(i)=sincos2(x2(i,:));
 end
 %Ieskosime min ir max
-[fMin,indMin]=min(f);
-[fMax,indMax]=max(f);
+[fMin2,indMin2]=min(f2);
+%[fMax2,indMax2]=max(f2);
 %Surandame, kurioje eiluteje yra musu ieskoma reiksme
-xMin=x(indMin,:);%paimama visa eilute su abiem reiksmem
-xMax=x(indMax,:);%paimama visa eilute su abiem reiksmem
+xMin2=x2(indMin2,:);%paimama visa eilute su abiejomis reiksmemis
+%xMax2=x2(indMax2,:);%paimama visa eilute su abiejomis reiksmemis
 %Surastas reiksmes isvedame ekrane
-fprintf('Surastas min=%6.4f, taske x=(%6.4f, %6.4f)\n',fMin,xMin(1),xMin(2));
-fprintf('Surastas max=%6.4f, taske x=(%6.4f, %6.4f)\n',fMax,xMax(1),xMax(2));
-grafikas2
+fprintf('Surastas min2=%6.4f, taske x2=(%6.4f, %6.4f)\n',fMin2,xMin2(1),xMin2(2));
+%fprintf('Surastas max2=%6.4f, taske x2=(%6.4f, %6.4f)\n',fMax,xMax(1),xMax(2));
+%grafikas2
+
+scatter(x2(:,1),x2(:,2),'g.');
+scatter(xMin2(1),xMin2(2),'r*');
+text(xMin2(1)+0.3,xMin2(2),num2str(fMin2));
+rectangle('Position',[xMin1(1)-4,xMin1(2)-4,8.0,8.0],...
+    'Linewidth',5,'Linestyle','--')
+
+if ((xMin(1)-4)>-10)&((xMin(1)+4)>10)&((xMin(2)-4)>10)&((xMin(2)+4)>1)
+
+
 
 
