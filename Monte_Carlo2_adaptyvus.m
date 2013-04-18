58941bbea97d6a9e4a36241a53a81c4ac87f601c
Dzeneta Buivydaite <dzeneciux@gmail.com>
2013-04-18 14:33:02 +0300

    Pradetas antrasis atsiskaitomas darbas ieskome geriausio intervalo

--- Monte_Carlo2_adaptyvus.m
+++ Monte_Carlo2_adaptyvus.m
@@ -1,16 +1,22 @@
+function [fMin2Visos,vidurk] = Monte_Carlo2_adaptyvus(funkcija,a1,b1)
+fMin2Visos = [];
+for ii=1:100
 %Monte Carlo (Random search) metodo realizacija.
 %Sugeneruojame 100 atsitiktiniu tasku is intervalo  [a,b].
 %Surandame kuriame taske funkcija igyja didziausia ir maziausia reiksme.
-a1 = -10; % Pradine sritis
-b1 = 10;
+%a1 = -10; % Pradine sritis
+%b1 = 10;
+%PALEIDIMAS: Pvz.:
+%a1=-10; b1=10; funkcija=@sincos2;
+%[fMin2,xMin2] = Monte_Carlo2_adaptyvus(funkcija,a1,b1)
 n=2;%Dimensija (matavimas)
-k1=50;%Tasku (vektoriu) skaicius, generuojame 50 tasku
+k1=80;%Tasku (vektoriu) skaicius, generuojame 50 tasku
 %Intervalas bus skaidomas i k1=50 ir k2=50
 %intervalas bus mazinamas
 x1 = a1 + (b1-a1) .* rand(k1,n); %Perdaryti, kad generuotu dvimacius vektorius
 f1=[];
 for i=1:k1
-f1(i)=sincos2(x1(i,:));%Imame visa eilute, perduodame reiksmes x1, x2
+f1(i)=funkcija(x1(i,:));%Imame visa eilute, perduodame reiksmes x1, x2
 end
 %Ieskosime min ir max
 [fMin1,indMin1]=min(f1);
@@ -19,7 +25,7 @@ end
 xMin1=x1(indMin1,:);%paimama visa eilute su abiejomis reiksmemis
 %xMax1=x1(indMax1,:);%paimama visa eilute su abiejomis reiksmemis
 %Surastas reiksmes isvedame ekrane
-fprintf('Surastas min1=%6.4f, taske x1=(%6.4f, %6.4f)\n',fMin1,xMin1(1),xMin1(2));
+%fprintf('Surastas min1=%6.4f, taske x1=(%6.4f, %6.4f)\n',fMin1,xMin1(1),xMin1(2));
 %fprintf('Surastas max1=%6.4f, taske x1=(%6.4f, %6.4f)\n',fMax1,xMax1(1),xMax1(2));
 %grafikas2
 
@@ -55,7 +61,7 @@ end
             end;
 
 %n=2;%dimensija(matavimas)
-k2=50;%likusiu tasku (vektoriu skaicius)
+k2=20;%likusiu tasku (vektoriu skaicius)
 x2(:,1)=a2+(b2-a2).*rand(k2,1);
 x2(:,2)=a3+(b3-a3).*rand(k2,1);
 
@@ -63,16 +69,18 @@ x2(:,2)=a3+(b3-a3).*rand(k2,1);
 
 f2=[];
 for i=1:k2
-    f2(i)=sincos2(x2(i,:));
+    f2(i)=funkcija(x2(i,:));
 end
 %Ieskosime min ir max
 [fMin2,indMin2]=min(f2);
+%fMin2Visos(ii) = fMin2;
+fMin2Visos = [fMin2Visos; fMin2]; 
 %[fMax2,indMax2]=max(f2);
 %Surandame, kurioje eiluteje yra musu ieskoma reiksme
 xMin2=x2(indMin2,:);%paimama visa eilute su abiejomis reiksmemis
 %xMax2=x2(indMax2,:);%paimama visa eilute su abiejomis reiksmemis
 %Surastas reiksmes isvedame ekrane
-fprintf('Surastas min2=%6.4f, taske x2=(%6.4f, %6.4f)\n',fMin2,xMin2(1),xMin2(2));
+     %fprintf('Surastas min2=%6.4f, taske x2=(%6.4f, %6.4f)\n',fMin2,xMin2(1),xMin2(2));
 %fprintf('Surastas max2=%6.4f, taske x2=(%6.4f, %6.4f)\n',fMax,xMax(1),xMax(2));
 %grafikas2
 
@@ -82,8 +90,8 @@ scatter(xMin2(1),xMin2(2),'r*');
 text(xMin2(1)+0.3,xMin2(2),num2str(fMin2));
 rectangle('Position',[a2,a3,8.0,8.0],...
     'Linewidth',5,'Linestyle','--')
-           
-
+end
+vidurk = mean(fMin2Visos);
 
     
 
---
+++ easom.m
@@ -0,0 +1,8 @@
+function y = easom(x)
+% 
+% Easom function 
+% Matlab Code by A. Hedar (Sep. 29, 2005).
+% The number of variables n = 2.
+% 
+format long
+y = -cos(x(1))*cos(x(2))*exp(-(x(1)-pi)^2-(x(2)-pi)^2);
